8a2cd0fcb2a6cda43a20abbd58ddeed1a1ccd7f4
unknown <sagarmikeylevi@gmail.com>
2023-05-03 21:02:28 +0530

    data fething with a loader

--- src/App.js
+++ src/App.js
@@ -17,7 +17,16 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
+          { index: true, element: <EventsPage />, loader: async () => {
+            const response = await fetch("http://localhost:8080/events");
+
+            if (!response.ok) {
+              // ... 
+            } else {
+              const resData = await response.json();
+              return resData.events;
+            }
+          }},
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -1,29 +1,9 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "Some event",
-  },
-  {
-    id: "e2",
-    title: "Another event",
-  },
-];
-
-const EventsPage = () => {
-  return (
-    <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
+function EventsPage() {
+  const events = useLoaderData();
+  return <EventsList events={events} />;
+}
 
 export default EventsPage;
