8ad999b34ba8f78e0d9cdb26c2da1b00fc703147
unknown <sagarmikeylevi@gmail.com>
2023-05-03 20:27:24 +0530

    setupp the basic structure

---
+++ src/pages/EditEventPage.js
@@ -0,0 +1,5 @@
+const EditEventPage = () => {
+    return <h1>EditEventPage</h1>;
+}
+
+export default EditEventPage;
---
+++ src/pages/NewEventPage.js
@@ -0,0 +1,5 @@
+const NewEventPage = () => {
+  return <h1>NewEventPage</h1>;
+};
+
+export default NewEventPage;
