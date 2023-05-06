c1515b302796470e06fc7bbcebb41f1fce261f10
unknown <sagarmikeylevi@gmail.com>
2023-05-06 16:34:21 +0530

    use the json utility function

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -9,7 +9,7 @@ const ErrorPage = () => {
   let message = "Something went wrong!";
 
   if (error.status === 500) {
-    message = JSON.parse(error.data).message;
+    message = error.data.message;
   }
 
   if (error.status === 404) {
--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
@@ -15,13 +15,16 @@ function EventsPage() {
 export default EventsPage;
 
 export const loader = async () => {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/events1");
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-      status: 500,
-    });
+
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+
+    throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     return response;
   }
