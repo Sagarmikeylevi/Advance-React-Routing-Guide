c88714013b3e576d686178875bb0f081e7e617fe
unknown <sagarmikeylevi@gmail.com>
2023-05-06 16:28:08 +0530

    extracting error data and throwing response

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import EventsPage, {loader as eventsLoader} from "./pages/EventsPage";
+import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
 import RootLayout from "./pages/Root";
 import EventRootLayout from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
         path: "events",
         element: <EventRootLayout />,
         children: [
-          { index: true, element: <EventsPage />, loader: eventsLoader},
+          { index: true, element: <EventsPage />, loader: eventsLoader },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -2,7 +2,13 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+
+  // if(data.isError) {
+  //   return <p>{data.message}</p>
+  // }
+
+  const events = data.events;
   return <EventsList events={events} />;
 }
 
@@ -12,9 +18,11 @@ export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 };
