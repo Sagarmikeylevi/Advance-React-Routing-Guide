ae962076baa7db4c9cc94a928f353fd416a8e27f
unknown <sagarmikeylevi@gmail.com>
2023-05-07 13:17:03 +0530

    validating user input and error validation

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,15 +1,26 @@
-import { useNavigate,  Form } from "react-router-dom";
+import { useNavigate, Form, useNavigation, useActionData } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
   function cancelHandler() {
     navigate("..");
   }
 
   return (
-    <Form method='post' className={classes.form}>
+    <Form method="post" className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
@@ -51,10 +62,12 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Save"}
+        </button>
       </div>
     </Form>
   );
--- src/pages/NewEventPage.js
+++ src/pages/NewEventPage.js
@@ -25,6 +25,10 @@ export const action = async ({ request, params }) => {
     body: JSON.stringify(eventData),
   });
 
+  if(response.status === 422) {
+    return response;
+  }
+
   if (!response.ok) {
     throw json({ message: "Could not save event." }, { status: 500 });
   }
